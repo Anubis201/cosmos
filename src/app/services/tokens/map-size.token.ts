b189206269e7c9c098bc8df1591cf23d14ebe916
Anubis201 <anubisanubias@gmail.com>
2021-12-19 18:04:03 +0100

    change size token

--- src/app/services/tokens/map-size.token.ts
+++ src/app/services/tokens/map-size.token.ts
@@ -4,6 +4,6 @@ export const TR_SIZE = new InjectionToken<number>('TrSize')
 export const TD_SIZE = new InjectionToken<number>('TDSize')
 
 export const mapSizeToken = [
-  { provide: TR_SIZE, useValue: 7 },
-  { provide: TD_SIZE, useValue: 8 },
+  { provide: TR_SIZE, useValue: 5 },
+  { provide: TD_SIZE, useValue: 6 },
 ]
